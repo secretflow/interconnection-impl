3b71f783d0379f7cbd343892cd9aac3275080c13
Strongman <none954@163.com>
2023-07-25 10:14:15 +0800

    new feature: add privacy-router for sf-lr-mix (#6)
    * add privacy-router-based interconnection
    
    * add apache license
    
    * rm DS_Store
    
    ---------
    
    Co-authored-by: chenlu <chenlu@tsingj.com>

---
+++ router/demo/horizontal_lr/ss_rc_01.py
@@ -0,0 +1,65 @@
+# Copyright 2023 Ant Group Co., Ltd.
+# Copyright 2023 Tsing Jiao Information Science
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from anyconn_core import AppConfig
+from router import RC, Pack
+import logging
+import numpy as np
+
+logger = logging.getLogger("anyconn")
+
+router_table = {
+    "rs_01": {"rs": ["rs_02"], "rc": ["rc_01", "rc_03"]},
+    "rs_02": {"rs": ["rs_01"], "rc": ["rc_02", "rc_03"]},
+}
+
+nodes = [
+    {"id": "rs_01", "tag": "RS", "address": "localhost:50051"},
+    {"id": "rs_02", "tag": "RS", "address": "localhost:50052"},
+    {"id": "rc_01", "tag": "RC01", "address": "localhost:50061"},
+    {"id": "rc_02", "tag": "RC02", "address": "localhost:50062"},
+    {"id": "rc_03", "tag": "RC03", "address": "localhost:50063"},
+]
+
+
+def run_ss_rc(_id):
+    rc = RC()
+    with rc.run_in_thread(AppConfig(node_id=_id, nodes=nodes)):
+        for my_round in range(4):
+            pack = rc.recv()[0]
+            data_slice = [d for d in pack.data]
+            logger.info(f"{_id} round{my_round} share:{data_slice}")
+
+            new_pack = Pack(
+                task_id='1',
+                source_id="rc_01",
+                data_id='2',
+                target_id="rs_01",
+                encryption="ss",
+                shape=np.shape(data_slice),
+                dtype="int",
+                data=data_slice,
+                process="ss2he",
+                key=None,
+                n_batches=2,
+                uid=f"round{my_round}_1",
+                router_table=router_table,
+            )
+            rc.send(new_pack)
+            logger.info(f"{_id} round{my_round} send back share")
+
+
+if __name__ == "__main__":
+    run_ss_rc("rc_01")
---
+++ router/demo/ss2he/he_rc_03.py
@@ -0,0 +1,74 @@
+# Copyright 2023 Ant Group Co., Ltd.
+# Copyright 2023 Tsing Jiao Information Science
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from anyconn_core import AppConfig
+from router import RC, Pack
+from heu import phe
+import pickle
+import logging
+
+logger = logging.getLogger("anyconn")
+
+router_table = {
+    "rs_01": {"rs": ["rs_02"], "rc": ["rc_01", "rc_03"]},
+    "rs_02": {"rs": ["rs_01"], "rc": ["rc_02", "rc_03"]},
+}
+
+nodes = [
+    {"id": "rs_01", "tag": "RS", "address": "localhost:50051"},
+    {"id": "rs_02", "tag": "RS", "address": "localhost:50052"},
+    {"id": "rc_01", "tag": "RC01", "address": "localhost:50061"},
+    {"id": "rc_02", "tag": "RC02", "address": "localhost:50062"},
+    {"id": "rc_03", "tag": "RC03", "address": "localhost:50063"},
+]
+
+
+def run_rc_03():
+    client_he = phe.setup(phe.SchemaType.ZPaillier, 2048)
+    public_key = client_he.public_key()
+    pk_buffer = pickle.dumps(public_key).decode('latin1')
+
+    rc = RC()
+    with rc.run_in_thread(AppConfig(node_id="rc_03", nodes=nodes)):
+        arr = [1]
+        new_pack = Pack(
+            task_id='1',
+            source_id="rc_03",
+            data_id='2',
+            target_id="rs_01",
+            encryption="he",
+            shape=(1,),
+            dtype="int",
+            data=arr,
+            process="ss2he",
+            key=pk_buffer,
+            n_batches=2,
+            uid="1",
+            router_table=router_table,
+        )
+        rc.send(new_pack)
+        logger.info("rc_03 send public_key pack.")
+
+        result = []
+        for pack in rc.recv():
+            res = [client_he.decryptor().decrypt_raw(pickle.loads(ct_buffer.encode("latin1"))) for ct_buffer in
+                   pack.data]
+            result.extend(res)
+        result = [data / 2 ** 40 for data in result]
+        logger.info(f"rc_03 recv: {result}")
+
+
+if __name__ == "__main__":
+    run_rc_03()
